06ffff2c40f5d3f39ea4e5d6b0f320e634a9a8c4
Karen Hernandez Sanchez <125052386@upq.edu.mx>
2026-09-18 17:03:43 -0600

    Avance Indivudal Practica No.2
    Avance individual del desarrollo de objetivos, se creo un documento .js de cocina y un .html para ejecutar en navegador

---
+++ CoffeeCode/cocina.js
@@ -0,0 +1,60 @@
+let productos = [];
+
+function agregar() {
+    let nombre = prompt("Nombre del producto:");
+    let precio = prompt("Precio del producto:");
+
+    productos.push(
+        {
+            nombre: nombre,
+            precio: precio
+        }
+    );
+}
+
+function listar() 
+{
+    let texto = "";
+
+    productos.forEach((producto, indice) => {
+        texto += `${indice + 1}. ${producto.nombre} - $${producto.precio}\n`;
+    });
+
+    alert(texto || "No hay productos");
+}
+
+function editar() {
+    listar();
+
+    let indice = prompt("Número del producto a editar:") - 1;
+
+    productos[indice].nombre = prompt("Nuevo nombre:");
+    productos[indice].precio = prompt("Nuevo precio:");
+}
+
+function eliminar() {
+    listar();
+
+    let indice = prompt("Número del producto a eliminar:") - 1;
+
+    productos.splice(indice, 1);
+}
+
+let opcion;
+
+do {
+        opcion = prompt(
+            "COCINA\n" +
+            "1. Agregar\n" +
+            "2. Editar\n" +
+            "3. Eliminar\n" +
+            "4. Listar\n" +
+            "5. Salir"
+        );
+
+    if (opcion == "1") agregar();
+    if (opcion == "2") editar();
+    if (opcion == "3") eliminar();
+    if (opcion == "4") listar();
+
+} while (opcion != "5");
