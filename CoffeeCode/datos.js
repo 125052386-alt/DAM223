3496f04d3586c18e1c346c870ed37517305b1c59
Karen Hernandez Sanchez <125052386@upq.edu.mx>
2026-09-19 21:01:11 -0600

    crear datos
    aqui solo se crean productos que se importan directo a listar

---
+++ CoffeeCode/datos.js
@@ -0,0 +1,16 @@
+export let productos = [
+    {
+        nombre: "Café",
+        precio: 25
+    },
+    {
+        nombre: "Capuchino",
+        precio: 30
+    },
+    {
+        nombre: "Té",
+        precio: 20
+    }
+];
+
+export const listaPedidos = [];
