e909e930789486d9586f3230a459a2f6f8aca434
Karen Hernandez Sanchez <125052386@upq.edu.mx>
2026-09-23 22:25:11 -0600

    Actualizacion de cajero
    Se agregaron los elementos reduce y destructuring

--- CoffeeCode/cajero.js
+++ CoffeeCode/cajero.js
@@ -3,14 +3,22 @@ import { listaPedidos } from "./datos.js";
 let totalAcumulado = 0;
 
 function agregarPedido(cliente, producto, precio) {
+    let preciosArray = [precio];
+    let subtotal = preciosArray.reduce((acumulador, actual) => acumulador + actual, 0);
+
+    let iva = subtotal * 0.16;
+    let precioConIva = subtotal + iva;
+
     let pedido = {
         nombreCliente: cliente,
         nombreProducto: producto,
-        precioProducto: precio
+        precioProducto: precioConIva,
+        subtotalPedido: subtotal,
+        ivaPedido: iva
     };
 
     listaPedidos.push(pedido);
-    totalAcumulado = totalAcumulado + precio;
+    totalAcumulado = totalAcumulado + precioConIva;
 
     console.log("¡Pedido agregado con éxito!");
 }
@@ -19,37 +27,32 @@ export function nuevoPedido(cliente) {
     let producto = prompt("Nombre del producto:");
     let precio = parseFloat(prompt("Precio del producto:"));
 
-    listaPedidos.push({
-        nombreCliente: cliente,
-        nombreProducto: producto,
-        precioProducto: precio
-    });
+    agregarPedido(cliente, producto, precio);
 
     alert("Pedido guardado");
 }
 
-
-
-function listarPedidos() {
-    nuevoPedido();
-
-    document.write("<h2>Lista de Pedidos</h2>");
+export function listarPedidos(cliente) {
+    let totalGeneral = 0;
+    let mensaje = "Lista de Pedidos - Caja\n\n";
 
     for (let i = 0; i < listaPedidos.length; i++) {
-        document.write(
-            "<p>Cliente: " + listaPedidos[i].nombreCliente + "</p>" +
-            "<p>Producto: " + listaPedidos[i].nombreProducto + "</p>" +
-            "<p>Precio: $" + listaPedidos[i].precioProducto.toFixed(2) + "</p>" +
-            "<hr>"
-        );
+        const { nombreCliente, nombreProducto, precioProducto, subtotalPedido, ivaPedido } = listaPedidos[i];
+        totalGeneral += precioProducto;
+
+        mensaje += "Ticket \n Cliente: " + nombreCliente + "\n" +
+            "Producto: " + nombreProducto + "\n" +
+            "Subtotal: $" + subtotalPedido.toFixed(2) + "\n" +
+            "IVA (16%): $" + ivaPedido.toFixed(2) + "\n" +
+            "Total con IVA: $" + precioProducto.toFixed(2) + "\n" +
+            "--------------------------------------\n\n";
     }
 
-    //mostrarPedidosCliente();
+    mensaje += "TOTAL DE TODOS LOS PRODUCTOS: $" + totalGeneral.toFixed(2);
+
+    alert(mensaje);
 
     console.log("--- LISTA DE PEDIDOS ---");
     console.log(listaPedidos);
-
-    console.log(
-        "Total acumulado en caja: $" + totalAcumulado.toFixed(2)
-    );
+    console.log("Total acumulado en caja: $" + totalGeneral.toFixed(2));
 }
