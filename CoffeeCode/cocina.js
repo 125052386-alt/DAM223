b975070600f86bf734475ab1313a38756f290a51
Karen Hernandez Sanchez <125052386@upq.edu.mx>
2026-09-23 21:56:26 -0600

    Actualización del codigo Cocina
    Se crearon la funciones filtar usando filter, buscar usando find y crearpromociones.

--- CoffeeCode/cocina.js
+++ CoffeeCode/cocina.js
@@ -1,5 +1,4 @@
-import { bebidas } from "./datos.js";
-import { postres } from "./datos.js";
+import { bebidas, postres, promociones } from "./datos.js";
 
 export function agregar() {
     let nombre = prompt("Nombre del producto:");
@@ -28,60 +27,149 @@ export function agregar() {
 export function listar() {
     let texto = "";
 
-    bebidas.forEach((bebida, indice) => {
-        texto += `${indice + 1}. ${bebida.nombre} - $${bebida.precio}\n`;
+    alert("Lista de productos disponibles:");
+    alert("Bebidas:");
+
+    bebidas.forEach((bebidas, indice) => {
+        texto += `${indice + 1}. ${bebidas.nombre} - $${bebidas.precio}\n`;
     });
 
     alert(texto || "No hay bebidas");
+    texto = "";
+
+    alert("Postres:");
+
+    postres.forEach((postres, indice) => {
+        texto += `${indice + 1}. ${postres.nombre} - $${postres.precio}\n`;
+    });
+
+    alert(texto || "No hay postres");
 }
 
 function editar() {
     listar();
+    let tipo = prompt("Tipo de producto a editar (bebida/postre):");
+    if (tipo.toLowerCase() === "bebida") {
+        let indice = prompt("Número del producto a editar:") - 1;
 
-    let indice = prompt("Número del producto a editar:") - 1;
+        bebidas[indice].nombre = prompt("Nuevo nombre:");
+        bebidas[indice].precio = prompt("Nuevo precio:");
+    }
+    else if (tipo.toLowerCase()==="postre");
+    {
+        let indice = prompt("Número del producto a editar:") - 1;
+        postres[indice].nombre = prompt("Nuevo nombre:");
+        postres[indice].precio = prompt("Nuevo precio:");
 
-    bebidas[indice].nombre = prompt("Nuevo nombre:");
-    bebidas[indice].precio = prompt("Nuevo precio:");
+    }
+
+
 }
 
 function eliminar() {
     listar();
+    
+    let tipo = prompt("Tipo de producto a eliminar (bebida/postre):");
+    if (tipo.toLowerCase() === "bebida") {
+        let indice = prompt("Número del producto a eliminar:") - 1;
 
-    let indice = prompt("Número del producto a eliminar:") - 1;
+        bebidas.splice(indice, 1);
+    }
+    else if (tipo.toLowerCase()==="postre");
+    {
+        let indice = prompt("Número del producto a eliminar:") - 1;
+        postres.splice(indice, 1);
 
-    productos.splice(indice, 1);
+    }
+
 }
-function filtar()
+export function filtrar()
 {
-    let baratos=productos.filter(producto => producto.precio < 30);
-    let texto = "Productos baratos:\n";
+    let texto = "";
+    const producto=[...bebidas,...postres];
 
-    baratos.forEach((producto, indice) => {
-        texto += `${indice + 1}. ${producto.nombre} - $${producto.precio}\n`;
-    });
+    do
+    {
+        let op=prompt("1.-Baratos \n 2.-Caros \n 3.-Bebidad \n"
+            +" 4.-Postres \n Elige una opción")
+        switch(op)
+        {
+            case "1":
+                let baratos=producto.filter(producto => producto.precio < 30);
+                let texto = "Productos baratos:\n";
 
-    alert(texto || "No hay productos baratos"); 
+                baratos.forEach((producto, indice) => {
+                    texto += `${indice + 1}. ${producto.nombre} - $${producto.precio}\n`;
+                });
 
-    let caros=productos.filter(roductos=>productos.precio>50);
-    let textoc="Productos caros:\n";
+                 alert(texto || "No hay productos baratos"); 
+                break
+            case "2": 
+                let caros=producto.filter(producto => producto.precio > 50);
+                let textoc="Productos caros:\n";
 
-    caros.forEach((producto, indice) => {
-        textoc += `${indice + 1}. ${producto.nombre} - $${producto.precio}\n`;
-    });
-    alert(textoc || "No hay productos caros");
+                caros.forEach((producto, indice) => {
+                 textoc += `${indice + 1}. ${producto.nombre} - $${producto.precio}\n`;
+                });
+
+                 alert(textoc || "No hay productos caros");
+                break
+            case "3":
+                alert("Bebidas:");
+
+                bebidas.forEach((bebidas, indice) => {
+                    texto += `${indice + 1}. ${bebidas.nombre} - $${bebidas.precio}\n`;
+                });
+
+                alert(texto || "No hay bebidas");
+                break
+            case "4": 
+                texto = "";
+
+                alert("Postres:");
+
+                postres.forEach((postres, indice) => {
+                texto += `${indice + 1}. ${postres.nombre} - $${postres.precio}\n`;
+                });
+
+                alert(texto || "No hay postres");
+                break
+            default:
+                alert("Opcion no disponible");
+        }
+
+    }while(op<0 && op>4);
+
+
+    
 }
-function buscar() {
+export function buscar() {
 
     let nombre = prompt("Ingrese el nombre del producto a buscar:");
-    let productoEncontrado=productos.find(producto => producto.nombre.toLowerCase() === nombre.toLowerCase());
+    let productoEncontradob=bebidas.find(bebida => bebida.nombre.toLowerCase() === nombre.toLowerCase());
+    let productoEncontradop=postres.find(postre => postre.nombre.toLowerCase() === nombre.toLowerCase());
 
-    if (productoEncontrado) {
-        alert(`Producto encontrado: ${productoEncontrado.nombre} - $${productoEncontrado.precio}`);
+    if (productoEncontradob) {
+        alert(`Producto encontrado: ${productoEncontradob.nombre} - $${productoEncontradob.precio}`);
+    } else if (productoEncontradop) {
+        alert(`Producto encontrado: ${productoEncontradop.nombre} - $${productoEncontradop.precio}`);
     } else {
         alert("Producto no encontrado");
     }
 
-}                                         
+}
+
+export function agregarPromocion() {
+    let descripcion = prompt("Escribe la nueva promoción:");
+
+    if (descripcion && descripcion.trim() !== "") {
+        promociones.push(descripcion.trim());
+        alert("Promoción agregada correctamente.");
+    } else {
+        alert("No se pudo agregar una promoción vacía.");
+    }
+}
+
 function mostrarMenu() {
     let opcion;
 
@@ -102,4 +190,8 @@ function mostrarMenu() {
 
     } while (opcion != "5");
 }
+/* filtrar();
+/* listar();
+
+buscar(); */
 
